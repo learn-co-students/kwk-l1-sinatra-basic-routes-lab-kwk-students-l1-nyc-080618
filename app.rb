b81e128c8044e522c15e1d4c367b82130d29f389
Sarah Ng <agbtr101a@gmail.com>
2018-08-14 15:26:51 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,4 +15,16 @@ end
 get '/song' do
   'My favorite song is "Mutual" by Shawn Mendes!'
 end
+
+get '/husband' do
+  'Sharah is endgame even if Shawn does not know it yet'
+end 
+
+get '/college' do
+  'I do not know where I am going yet'
+end
+
+get '/hi' do
+  'hi'
+end
 end 
