9fd15c964ba112b4ffe679cf9bc666f43b34e4d9
kazinur <kazinur@mybihs.org>
2018-08-14 15:26:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    'Welcome to my page!'
+    #it means this is your homepage 
+  end 
+  
+                    get'/name' do 
+                      'Hi! My name is Kazi Nur'
+                    end 
+  
+                              get '/hometown' do 
+                                "My hometown is Queens, NY"
+                              end 
+  
+                                      get '/favorite-song' do 
+                                        "My favorite song is Back To You"
+                                      end 
 end
+
+
