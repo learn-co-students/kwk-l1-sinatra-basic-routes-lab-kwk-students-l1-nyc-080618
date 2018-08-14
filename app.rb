67d2370d2ddd91a643e3bdda5f11041c2f711a0a
christinasherpa <chrisher24@charter.newvisions.org>
2018-08-14 15:26:42 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,28 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Welcome to my page'
+  end 
+  
+  get'/name' do
+    'Hi my name is Christina'
+  end
+  
+  get'/hometown' do 
+    'I am from Queens, NY'
+  end
+  
+  get '/favesong' do
+    'I do not have a fave song at this point'
+  end 
+  
+  get'/mood' do
+    'I am super sleeeeeeepy right now! NEED COFFEE ASAP!'
+  end
+  
+  get '/favefood' do
+    'korean bbq?'
+  end
+  
 end
