21588822a6a141b416d0456d7e88b0e274a41e21
usernameapril <18mata.a@twain239.org>
2018-08-14 15:26:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/'do
+    'welcome to my page'
+end
+get '/city' do
+  'England is my city'
+end
+get '/song' do
+  'my favorite song is star'
+end
+get '/home' do
+end
 end
