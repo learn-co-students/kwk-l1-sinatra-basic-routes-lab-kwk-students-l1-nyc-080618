0853e80e2b411afe4f12e7e6f7cecbcf4cf83838
Hilary Zen <hilary.zen@gmail.com>
2018-08-14 15:26:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,31 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    'Welcome to my page!'
+  end
+  
+  get '/name' do
+    'My name is Hilary!'
+  end
+  
+  get '/hometown' do
+    'My hometown is New York City.'
+  end
+  
+  get '/favorite-song' do
+    'My favorite song is Dangerous Woman.'
+  end
+  
+  get '/school' do
+    'I go to Stuyvesant High School.'
+  end
+  
+  get '/siblings' do
+    'I have a younger brother.'
+  end
+  
+  get '/favorite-ice-cream' do
+    'My favorite ice cream flavor is chocolate.'
+  end
 end
