ca4587260af06f6b006ab08a8bb78c18d1788062
Sarah Ng <agbtr101a@gmail.com>
2018-08-14 15:24:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+get '/' do 
+'Welcome to my page!'
 end
+get '/name' do
+'My name is bla bla bla'
+end
+
+get '/hometown' do
+  'My hometown is Queens, NY!'
+end
+
+get '/song' do
+  'My favorite song is "Mutual" by Shawn Mendes!'
+end
+end 
