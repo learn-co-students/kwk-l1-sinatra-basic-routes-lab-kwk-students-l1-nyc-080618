947816180ba25c0398bb9cc7a45bfa2e742045ee
YingqiTao <yingqitao2000@gmail.com>
2018-08-14 15:27:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    #create a website
+    'Welcome to my page'
+  end
+  
+  get '/name' do
+    'Hello, my name is Wendy!'
+  end
+  
+  get '/hometown' do
+    'My hometown is Guangzhou, China.'
+  end
+  
+  get '/my_fav_thing' do
+    'My favorite thing is a laptop'
+  end
 end
